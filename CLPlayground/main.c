991c0de12a242b87507dc66cf5ed2d03bee06259
Magnus Morton <magnusmorton@icloud.com>
2016-01-28 22:01:50 +0000

    Initial Commit

---
+++ CLPlayground/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CLPlayground
+//
+//  Created by John Morton on 28/01/2016.
+//  Copyright © 2016 Magnus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
